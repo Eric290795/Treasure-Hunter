017c670986c312403213d38848a5e3752d658e15
erictran <erictran@172.22.130.231>
2019-06-04 14:10:53 +0200

    Create map with item in class Map

--- Treasure_Hunter/src/main/java/com/eric_treasure/Map.java
+++ Treasure_Hunter/src/main/java/com/eric_treasure/Map.java
@@ -1,32 +1,76 @@
 package com.eric_treasure;
 
+import java.io.IOException;
 import java.util.Objects;
 
 public class Map {
 	
-	private final int row;
-	private final int col;
+	private final int rows;
+	private final int cols;
 	private final Item[][] items;
 
-	public Map(int row, int col) {
-		this.row = Objects.requireNonNull(row);
-		this.col = Objects.requireNonNull(col);
-		this.items = new Item[row][col];
+	public Map(int rows, int cols) {
+		this.rows = Objects.requireNonNull(rows);
+		this.cols = Objects.requireNonNull(cols);
+		this.items = new Item[rows][cols];
 	}
 	
 	public void print() {
 		System.out.print("  ");
-		for(var i = 0; i < col; i++) {
+		for(var i = 0; i < cols; i++) {
 			System.out.print(i+1 + " ");
 		}
 		System.out.println();
-		for(var i = 0; i < row; i++) {
+		for(var i = 0; i < rows; i++) {
 			System.out.print(i+1);
-			for(var j = 0; j < col; j++) {
+			for(var j = 0; j < cols; j++) {
 				System.out.print("| ");
 			}
 			System.out.print("|");
 			System.out.println();
 		}
 	}
+	
+	public void set(int r, int c, Item item) {
+		if(r < 0 || r >= this.rows || c < 0 || c >= this.cols) {
+			throw new IndexOutOfBoundsException();
+		}
+		if(item == null) {
+			throw new IllegalArgumentException("item cannot be null");
+		}
+		this.items[r][c] = item;
+	}
+	
+	public Map create(String path) throws IOException {
+		
+		var lines = FileToList.load(path);
+		Map map = null;
+		String[] tokens;
+		String[] position;
+		
+		for(var line : lines) {
+			tokens = line.split(" ");
+			if(map == null){
+				map = new Map(Integer.parseInt(tokens[2]),Integer.parseInt(tokens[1]));
+			}
+			else {
+				position = tokens[1].split("-");
+				var col = Integer.parseInt(position[0]) - 1;
+				var row = Integer.parseInt(position[1]) - 1;
+				
+				switch (tokens[0]) {
+				case "T" :
+					map.set(row, col, new Treasure(Integer.parseInt(tokens[2])));
+					break;
+				case "M" :
+					map.set(row, col, new Mountain());
+					break;
+				default :
+					throw new IllegalStateException("malformed line " + line);
+				}
+			}
+		}
+		return map;
+	}
+	
 }
